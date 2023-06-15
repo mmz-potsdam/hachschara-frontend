f4327ccf827aa687b4b1d0a0417a1107bd809039
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2023-06-15 11:41:31 +0200

    Switch to knp_menu_render

---
+++ src/Menu/Builder.php
@@ -0,0 +1,191 @@
+<?php
+// src/Menu/Builder.php
+
+// see http://symfony.com/doc/current/bundles/KnpMenuBundle/index.html
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class Builder
+{
+    private $factory;
+    private $translator;
+    private $requestStack;
+    private $router;
+
+    /**
+     * @param FactoryInterface $factory
+     * @param TranslatorInterface $translator
+     * @param RequestStack $requestStack
+     * @param Router $router
+     *
+     * Add any other dependency you need
+     */
+    public function __construct(FactoryInterface $factory,
+                                TranslatorInterface $translator,
+                                RequestStack $requestStack,
+                                RouterInterface $router)
+    {
+        $this->factory = $factory;
+        $this->translator = $translator;
+        $this->requestStack = $requestStack;
+        $this->router = $router;
+    }
+
+    public function createMainMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('main', [
+            'childrenAttributes' => [
+                'id' => 'menu-main',
+                'class' => 'container navbar-nav nav-fill w-100 ms-auto flex-nowrap',
+            ],
+        ]);
+
+        // add menu items
+        $menu->addChild('site', [
+            'label' => $this->translator->trans('Sites'),
+            'route' => 'site-map',
+            'attributes' => [
+                'class' => 'nav-item',
+            ],
+            'linkAttributes' => [
+                'class' => 'nav-link',
+            ],
+        ]);
+
+        $menu->addChild('lookup', [
+            'label' => $this->translator->trans('Look-up'),
+            'uri' => '#',
+            'attributes' => [
+                'id' => 'dropdownLookupMenuButton',
+                'class' => 'nav-item dropdown',
+            ],
+            'linkAttributes' => [
+                'class' => 'nav-link dropdown-toggle',
+                'dropdown' => true,
+                'role' => 'button',
+                'data-bs-toggle' => 'dropdown',
+                'aria-expanded' => 'false',
+            ],
+            'childrenAttributes' => [
+                'class' => 'dropdown-menu dropdown-menu-center',
+                'aria-labelledby' => 'dropdownLookupMenuButton',
+            ],
+        ]);
+
+        $menu['lookup']
+            ->addChild('person', [
+                'label' => $this->translator->trans('Persons'),
+                'route' => 'person-index',
+                'linkAttributes' => [
+                    'class' => 'dropdown-item',
+                ],
+            ]);
+
+        $menu['lookup']
+            ->addChild('organization', [
+                'label' => $this->translator->trans('Organizations'),
+                'route' => 'organization-index',
+                'linkAttributes' => [
+                    'class' => 'dropdown-item',
+                ],
+            ]);
+
+        $menu['lookup']
+            ->addChild('glossary', [
+                'label' => $this->translator->trans('Glossary'),
+                'route' => 'glossary-index',
+                'linkAttributes' => [
+                    'class' => 'dropdown-item',
+                ],
+            ]);
+
+        $menu['lookup']
+            ->addChild('bibliography', [
+                'label' => $this->translator->trans('Literature'),
+                'route' => 'bibliography-index',
+                'linkAttributes' => [
+                    'class' => 'dropdown-item',
+                ],
+            ]);
+
+        $menu->addChild('about', [
+            'label' => $this->translator->trans('Über'),
+            'route' => 'about',
+            'attributes' => [
+                'id' => 'dropdownAboutMenuButton',
+                'class' => 'nav-item dropdown',
+            ],
+            'linkAttributes' => [
+                'class' => 'nav-link dropdown-toggle',
+                'dropdown' => true,
+                'role' => 'button',
+                'data-bs-toggle' => 'dropdown',
+                'aria-expanded' => 'false',
+            ],
+            'childrenAttributes' => [
+                'class' => 'dropdown-menu dropdown-menu-center',
+                'aria-labelledby' => 'dropdownAboutMenuButton',
+            ],
+        ]);
+
+        $menu['about']
+            ->addChild('about-hakhshara', [
+                'label' => $this->translator->trans('Hakhshara'),
+                'route' => 'about-hakhshara',
+                'linkAttributes' => [
+                    'class' => 'dropdown-item',
+                ],
+            ]);
+
+        $menu['about']
+            ->addChild('about', [
+                'label' => $this->translator->trans('The Project'),
+                'route' => 'about',
+                'linkAttributes' => [
+                    'class' => 'dropdown-item',
+                ],
+            ]);
+
+        $menu['about']
+            ->addChild('about-network', [
+                'label' => $this->translator->trans('The Network'),
+                'route' => 'about-network',
+                'linkAttributes' => [
+                    'class' => 'dropdown-item',
+                ],
+            ]);
+
+        $menu['about']
+            ->addChild('imprint', [
+                'label' => $this->translator->trans('Imprint'),
+                'uri' => $this->router->generate('imprint') . '#imprint',
+                'linkAttributes' => [
+                    'class' => 'dropdown-item',
+                ],
+            ]);
+
+        // attempt to set the current item for hierarchical entries
+        $currentRoute = $this->requestStack->getCurrentRequest()->get('_route');
+
+        if (!is_null($currentRoute)) {
+            if (preg_match('/^(site|about)/', $currentRoute, $matches)) {
+                $menu[$matches[1]]->setCurrent(true);
+            }
+            else if (preg_match('/^(person|organization|glossary|bibliography)/', $currentRoute, $matches)) {
+                $menu['lookup']->setCurrent(true);
+            }
+            else if (preg_match('/^(user|imprint)/', $currentRoute, $matches)) {
+                $menu['about']->setCurrent(true);
+            }
+        }
+
+        return $menu;
+    }
+}
---
+++ src/Menu/Renderer.php
@@ -0,0 +1,71 @@
+<?php
+// src/Menu/Renderer.php
+
+// see https://symfony.com/bundles/KnpMenuBundle/current/custom_renderer.html
+namespace App\Menu;
+
+use Knp\Menu\ItemInterface;
+use Knp\Menu\Matcher\MatcherInterface;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+
+class Renderer extends \Knp\Menu\Renderer\ListRenderer
+{
+    protected $requestStack;
+    protected $router;
+    protected $locales;
+
+    public function __construct(MatcherInterface $matcher,
+                                array $defaultOptions = [],
+                                string $charset = null,
+                                RequestStack $requestStack,
+                                RouterInterface $router,
+                                array $locales = [])
+    {
+        $this->requestStack = $requestStack;
+        $this->router = $router;
+        $this->locales = $locales;
+
+        parent::__construct($matcher, $defaultOptions, $charset);
+    }
+
+    /**
+     * Override to prepend the locale-switch for mobile view
+     */
+    protected function renderChildren(ItemInterface $item, array $options): string
+    {
+        $prepend = '';
+
+        if ('main' == $item->getName() && count($this->locales) > 1) {
+            $request = $this->requestStack->getCurrentRequest();
+
+            $currentLocale = $request->getLocale();
+
+            $routeName = $request->attributes->get('_route');
+            $routeParameters = $request->attributes->get('_route_params');
+
+            //
+            $prepend = '<li class="nav-item d-md-none"><ul class="text-end list-inline">';
+            for ($i = 0; $i < count($this->locales); $i++) {
+                $last = $i == count($this->locales) - 1;
+
+                $locale = $this->locales[$i];
+                $routeParameters['_locale'] = $locale;
+
+                $a = sprintf('<a class="nav-link%s" href="%s">%s</a>',
+                             $last ? '' : ' divider',
+                             $this->router->generate($routeName, $routeParameters),
+                             mb_strtoupper($locale, 'UTF-8'));
+
+                $prepend .= sprintf('<li class="list-inline-item%s">%s</li>',
+                                    $locale == $currentLocale ? ' active' : '',
+                                    $a);
+            }
+
+            $prepend .= '</ul></li>';
+        }
+
+        return $prepend . parent::renderChildren($item, $options);
+    }
+}
